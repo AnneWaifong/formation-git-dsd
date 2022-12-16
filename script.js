631b8aa579af56439f8f31806f5e30aa598f5463
Waifong <waifonglee72@gmail.com>
2022-12-16 12:10:20 +0100

    ahoute Circle Rectangle

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 document.querySelector('.box').textContent = 'Box';
 document.querySelector('.box').textContent = 'Square';
+document.querySelector('.box').textContent = 'Circle';
+document.querySelector('.box').textContent = 'Rectangle';
